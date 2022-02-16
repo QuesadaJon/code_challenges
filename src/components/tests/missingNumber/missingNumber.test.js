25b4f2fdf73e9339779415fcf881af29d84d87ee
Jonathan Quesada <jonnythan.quesada@gmail.com>
2022-02-16 13:39:48 -0800

    add missingNumber

--- src/components/tests/missingNumber/missingNumber.test.js
+++ src/components/tests/missingNumber/missingNumber.test.js
@@ -4,7 +4,11 @@ describe('missingNumber', () => {
   it('returns the only number in the range that is missing from the array', 
     () => {
       const test = [3, 0, 1];
+      const test2 = [0, 1];
+      const test3 = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 
       expect(missingNumber(test)).toEqual(2);
+      expect(missingNumber(test2)).toEqual(2);
+      expect(missingNumber(test3)).toEqual(8);
     });
 });
